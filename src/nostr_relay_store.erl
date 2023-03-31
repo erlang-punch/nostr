c4ac0829dcd754915e448da7e8c7032322699801
niamkik <niamkik@localhost.localdomain>
2023-03-31 17:25:53 +0200

    add store documentation and some test with/for mnesia

--- src/nostr_relay_store.erl
+++ src/nostr_relay_store.erl
@@ -1,5 +1,79 @@
 %%%===================================================================
-%%% @doc
+%%% @doc THIS IS A DRAFT
+%%%
+%%% ```
+%%% rr(nostrlib).
+%%% mnesia:start().
+%%%
+%%% % create a new table
+%%% mnesia:create_table(event, [{attributes, record_info(fields, event)}]).
+%%%
+%%% % create a new event
+%%% {ok, E2} 
+%%%   = nostrlib:encode(#event{content = <<"test">>, kind = text_note},
+%%%      [{private_key, <<1:256>>}, {as_record, true}]).
+%%%
+%%% % insert this event in the table
+%%% _ = mnesia:transaction(fun() -> mnesia:write(E2) end).
+%%%
+%%% % search the value by the id of the event
+%%% % returns only the content of this event
+%%% mnesia:transaction(fun() ->
+%%%   Search = #event{ id = E2#event.id
+%%%                  , content = '$1'
+%%%                  , created_at = '_'
+%%%                  , signature = '_'
+%%%                  , kind = '_'
+%%%                  , public_key = '_' }
+%%%   mnesia:select(event, [{Search,[],['$1']}])
+%%% end)
+%%%
+%%% % search the value by the id of the event
+%%% % returns only the content of this event
+%%% mnesia:transaction(fun() ->
+%%%   Search = #event{ id = E2#event.id
+%%%                  , content = '$1'
+%%%                  , created_at = '_'
+%%%                  , signature = '_'
+%%%                  , kind = '_'
+%%%                  , public_key = '$2' }
+%%%   mnesia:select(event, [{Search,[],[['$1', '$2']]}])
+%%% end)
+%%%
+%%% Prefix =:= erlang:binary_part(E2#event.id, {0, 8}).
+%%%
+%%% % convert a fun to a match spec
+%%% ets:fun2ms(
+%%%    fun(#event{ id = Id
+%%%              , content = Content
+%%%              , created_at = _
+%%%              , signature = _
+%%%              , kind = _, public_key = _}) 
+%%%      when Prefix =:= binary_part(Id, {0,2}) -> 
+%%%        Content 
+%%%    end).
+%%%
+%%% % Pattern Spec to extract the whole content
+%%% % [{#event{id = '$1', public_key = '_', created_at = '_',
+%%% %          kind = '_', tags = '_', content = '$2', signature = '_'},  
+%%% %  [{'=:=',{const,<<202,244,196,122,235,105,92,155>>},
+%%% %          {binary_part,'$1',{{0,8}}}
+%%% %   }],
+%%%
+%%% % lambda function to extract a part of an id.
+%%% fun(Prefix) ->
+%%%   mnesia:transaction(fun() -> 
+%%%     Event = #event{ id = '$2', content = '$1', created_at = '_'
+%%%                   , signature = '_', kind = '_', public_key = '_'},
+%%%     Guard = [{'=:=',{const, Prefix}
+%%%                    ,{binary_part,'$2',{{0,8}}}
+%%%             }],
+%%%     Select = ['$1'],
+%%%     mnesia:select(event, [{Event, Guard, Select}]) 
+%%%   end)
+%%% end.
+%%% '''
+%%%
 %%% @end
 %%%===================================================================
 -module(nostr_relay_store).
