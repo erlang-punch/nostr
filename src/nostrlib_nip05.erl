34af66f5293ab8e8e66361f8d3e02fd9e5f6bdc4
niamkik <niamkik@localhost.localdomain>
2023-05-09 21:39:16 +0200

    == 2023-05-09: Shorcut_23/19
    Hi everyone! The week was a bit exausting on my side... So, I will try
    to continue the previous implementation of nip/05. First, we will read
    the change during the last session. Okay! Let's go!
    
     + created check_json/1 function to check json object returned by the
       server hosting the information about one user
     + added eunit test support
     + added more documentation
     + added some todos.
     ! require documentation on many functions
     ! need to check the data stored in names and relays attributes.
    
    == 2023-05-09: Shorcut_23/20
    
    Did some cleanup, in particular in the init function.
    
     + splitted the init function in small functions to check
       arguments present in a proplists
     + added module documentation
     + added more documentation
     ! need to rename this module to nostr_client_nip05
    
    == 2023-05-09: Shorcut_23/21
    
    This last part was dedicated to test the process... And it does not
    work correctly. Anyway, it will be fixed next time. When creating a
    connection to the remote server with a valid domain and path, nothing
    is returned. It's possibly related to CA validation (to check).
    
    Anyway, see you Friday... :)
    
     + add logging support
     + did some tests
     ~ fixed a bug in the path used to fetch the information
     ~ fixed few other bugs

--- src/nostrlib_nip05.erl
+++ src/nostrlib_nip05.erl
@@ -1,25 +1,51 @@
-%%%-------------------------------------------------------------------
-%%% @doc DRAFT
+%%%===================================================================
+%%% @doc `nostrlib_nip05' implements NIP/05 specification for the
+%%% client client. When a client receives a kind 0 event containing a
+%%% `pubkey' and a `content' attribute with `nip05' fields in it, it
+%%% should check if the user is correct.
+%%%
+%%% ```
+%%% % to test: https://nostrprotocol.net/.well-known/nostr.json?name=bob
+%%% '''
+%%%
+%%% see: https://github.com/nostr-protocol/nips/blob/master/05.md
+%%%
+%%% @todo improve specifications
+%%% @todo rename this module: nostr_client_nip05.
+%%% @todo the state present in this process should be stored somewhere
+%%%       else (database).
 %%% @end
-%%%-------------------------------------------------------------------
+%%%===================================================================
 -module(nostrlib_nip05).
 -behavior(gen_statem).
 -export([start/1]).
 -export([init/1]). %, terminate/3]).
 -export([callback_mode/0]).
 -export([wait/3, response/3, done/3]).
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("kernel/include/logger.hrl").
 -record(state, { target = undefined
+               , public_key = undefined :: undefined | binary() | bitstring()
                , connection = undefined
                , connection_reference = undefined
+               , valid = false :: boolean()
                }).
+
+%%--------------------------------------------------------------------
+%% required for eunit.
+%%--------------------------------------------------------------------
+-spec test() -> any().
 
 %%--------------------------------------------------------------------
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
--spec start(binary()) -> {ok, pid()}.
-start(Identifier) ->
-    gen_statem:start(?MODULE, Identifier, []).
+-spec start(Opts) -> Return when
+      Opts :: proplists:proplists(),
+      Return :: {ok, pid()}.
+
+start(Opts) ->
+    gen_statem:start(?MODULE, Opts, []).
 
 -spec callback_mode() -> [atom()].
 callback_mode() -> [state_functions, state_enter].
@@ -33,22 +59,75 @@ callback_mode() -> [state_functions, state_enter].
 %%
 %% @end
 %%--------------------------------------------------------------------
--spec init(binary()) -> {ok, wait, #state{}}.
-init(Identifier) ->
-    % 1. load the certificates
-    ok = public_key:cacerts_loads(),
+-spec init(Opts) -> Return when 
+      Opts :: proplists:proplists(),
+      Return :: {ok, wait, #state{}}.
 
-    % 2. retrieve the certificates
-    % @todo: add certificate support
-    _Certs = public_key:cacerts_get(),
+init(Opts) ->
+    logger:set_module_level(?MODULE, debug),
+    State = #state{},
+    init_identifier(Opts, State).
 
-    {ok, Target} = new(Identifier),
-    {ok, Connection} = open(Target),
+%%--------------------------------------------------------------------
+%% @doc internal.
+%%
+%% check if the identifier is present. If it's the case, convert it as
+%% target.
+%% 
+%% @todo manage exception when creating the target.
+%% @end
+%%--------------------------------------------------------------------
+init_identifier(Opts, State) ->
+    case proplists:get_value(identifier, Opts, undefined) of
+        undefined -> 
+            ?LOG_ERROR("~p: missing identifier", [{?MODULE, self()}]),
+            {stop, [{message, <<"missing identifier">>}]};
+        Identifier -> 
+            {ok, Target} = new(Identifier),
+            NewState = State#state{ target = Target },
+            init_public_key(Opts, NewState)
+    end.
 
-    State = #state{ target = Target
-                  , connection = Connection
-                  },
-    {ok, wait, State}.
+%%--------------------------------------------------------------------
+%% @doc internal.
+%%
+%% A public key is required there, the content from the server should
+%% be compared to it.
+%% 
+%% @todo validated the public key passed in opts.
+%% @end
+%%--------------------------------------------------------------------
+init_public_key(Opts, State) ->
+    case proplists:get_value(public_key, Opts, undefined) of
+        undefined ->
+            ?LOG_ERROR("~p: missing public_key", [{?MODULE, self()}]),
+            {stop, [{message, <<"missing public_key">>}]};
+        PublicKey when is_binary(PublicKey) ->
+            NewState = State#state{ public_key = PublicKey },
+            init_connection(NewState);
+        _Elsewise ->
+            ?LOG_ERROR("~p: invalid public_key", [{?MODULE, self()}]),
+            {stop, [{message, <<"invalid public_key">>}]}
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc internal.
+%%
+%% initialize the connection with gun:open wrapper (open/1) function
+%% and start the main process loop.
+%%
+%% @end
+%%--------------------------------------------------------------------
+init_connection(#state{ target = Target } = State) ->
+    case open(Target) of
+        {ok, Connection} ->
+            ?LOG_DEBUG("~p: successfully connected to ~p", [{?MODULE, self()}, Target]),
+            NewState = State#state{ connection = Connection },
+            {ok, wait, NewState};
+        {error, Reason} ->
+            ?LOG_ERROR("~p: can't connect", [{?MODULE, self()}]),
+            {stop, [{reason, Reason}]}
+    end.
 
 %%--------------------------------------------------------------------
 %% @doc internal.
@@ -65,16 +144,23 @@ init(Identifier) ->
       Return :: tuple().
           
 wait(enter, _, #state{ target = #{ path := Path }
-                     , connection = Connection } = State) ->
+                     , connection = Connection } = State) ->    
     ConnectionReference = gun:get(Connection, Path),
+    ?LOG_DEBUG("~p: get path ~p", [{?MODULE, self(), State}, Path]),
     NewState = State#state{ connection_reference = ConnectionReference },
     {next_state, wait, NewState};
 wait(info, {gun_response, _Conn, _Ref, fin, _Status, _Headers}, State) ->
+    ?LOG_DEBUG("~p: received ~p", [{?MODULE, self()}, fin]),
     {next_state, done, State};
 wait(info, {gun_response, _Conn, _Ref, nofin, _Status, _Headers}, State) ->
+    ?LOG_DEBUG("~p: received ~p", [{?MODULE, self()}, nofin]),
     {next_state, response, State};
-wait(info, {'DOWN', _MRef, process, _Conn, _Reason}, State) ->
-    {next_state, done, State}.
+wait(info, {'DOWN', _MRef, process, ConnPid, Reason}, State) ->
+    ?LOG_DEBUG("~p: process ~p down with ~p", [{?MODULE, self()}, ConnPid, Reason]),
+    {next_state, done, State};
+wait(_, Message, State) ->
+    ?LOG_DEBUG("~p, received ~p", [{?MODULE, self()}, Message]),
+    {keep_state, State}.
 
 %%--------------------------------------------------------------------
 %% @doc internal.
@@ -89,12 +175,18 @@ wait(info, {'DOWN', _MRef, process, _Conn, _Reason}, State) ->
       Data :: #state{},
       Return :: tuple().
 
-response(info, {gun_data, _Conn, _Ref, nofin, _Data}, State) ->
+response(info, {gun_data, _Conn, _Ref, nofin, Data}, State) ->
+    ?LOG_DEBUG("~p: received ~p", [{?MODULE, self()}, {nofin, Data}]),
     {keep_state, State};
-response(info, {gun_data, _Conn, _Ref, fin, _Data}, State) ->
+response(info, {gun_data, _Conn, _Ref, fin, Data}, State) ->
+    ?LOG_DEBUG("~p: received ~p", [{?MODULE, self()}, {fin, Data}]),
     {next_state, done, State};
-response(info, {'DOWN', _MRef, process, _ConnPid, _Reason}, State) ->
-    {next_state, done, State}.
+response(info, {'DOWN', _MRef, process, ConnPid, Reason}, State) ->
+    ?LOG_DEBUG("~p: process ~p down with ~p", [{?MODULE, self()}, ConnPid, Reason]),
+    {next_state, done, State};
+response(_, Message, State) ->
+    ?LOG_DEBUG("~p, received ~p", [{?MODULE, self()}, Message]),
+    {keep_state, State}.
 
 %%--------------------------------------------------------------------
 %% @doc internal.
@@ -111,13 +203,16 @@ response(info, {'DOWN', _MRef, process, _ConnPid, _Reason}, State) ->
       Return :: tuple().
 
 done(enter, _, State) ->
+    ?LOG_DEBUG("~p: entered done state", [{?MODULE, self()}]),
     {next_state, done, State};
 done({call, _From}, get, State) ->
+    ?LOG_DEBUG("~p: received get", [{?MODULE, self()}]),
     {keep_state, State}.
 
 %%--------------------------------------------------------------------
 %% @hidden
-%% @doc
+%% @doc internal.
+%%
 %% @end
 %%--------------------------------------------------------------------
 -spec new(binary()) -> any().
@@ -136,6 +231,12 @@ new(Identifier) ->
             {error, Result}
     end.
 
+%%--------------------------------------------------------------------
+%% @doc internal.
+%%
+%% @todo add documentation
+%% @end
+%%--------------------------------------------------------------------
 target(#{ <<"localpart">> := LocalPart 
               , <<"domain">> := Domain
               } = Identifier) ->
@@ -147,22 +248,124 @@ target(#{ <<"localpart">> := LocalPart
                           path => Path },
     {ok, Return}.
 
+%%--------------------------------------------------------------------
+%% @doc internal.
+%%
+%% @todo add documentation
+%% @end
+%%--------------------------------------------------------------------
 uri(LocalPart, Domain) ->
     URI = #{ scheme => <<"https">>,
              host => Domain,
-             path => <<".well-known/nostr.json">>,
+             path => <<"/.well-known/nostr.json">>,
              query => <<"name=",LocalPart/bitstring>>
            },
     {ok, URI}.
 
+%%--------------------------------------------------------------------
+%% @doc internal.
+%%
+%% @todo add documentation
+%% @end
+%%--------------------------------------------------------------------
 url(URI) ->
     {ok, uri_string:recompose(URI)}.
 
+%%--------------------------------------------------------------------
+%% @doc internal.
+%%
+%% @todo add documentation
+%% @end
+%%--------------------------------------------------------------------
 path(#{ path := Path, query := Query }) ->
     {ok, uri_string:recompose(#{ path => Path, query => Query })}.
 
-open(#{ domain := Domain }) ->
+%%--------------------------------------------------------------------
+%% @doc internal.
+%%
+%% Opens a new HTTP connection to a remote server by using only HTTPS
+%% with verified certificates.
+%%
+%% @end
+%%--------------------------------------------------------------------
+open(#{ <<"domain">> := Domain } = _Target) ->
+    ok = public_key:cacerts_load(),
+    Certs = public_key:cacerts_get(),
     DomainList = binary_to_list(Domain),
-    gun:open(DomainList, 443).
-    
-    
+    TlsOpts = [{cacerts, Certs}
+              ,{verify, verify_peer}],
+    GunOpts = #{ tls_opts => TlsOpts
+               % @todo fix this timeout, to something more acceptable!  
+               % , tls_handshake_timeout => 3
+               , transport => tls
+               },
+    gun:open(DomainList, 443, GunOpts).
+
+%%--------------------------------------------------------------------
+%% @doc internal.
+%%
+%% parse the data received from gun. It's a wrapper around
+%% thoas:decode/1 function.
+%%
+%% @todo improve coverage on this function.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec parse_json(Data) -> Return when
+      Data :: bitstring() | binary(),
+      Return :: {ok, map()} | {error, Reason},
+      Reason :: proplist:proplists().
+
+parse_json(Data) ->
+    case thoas:decode(Data) of
+        {ok, Result} ->
+            check_json_names(Result, #{});
+        Elsewise -> Elsewise
+    end.
+
+-spec check_json_test() -> any().
+check_json_test() ->
+    BobKey = <<"b0635d6a9851d3aed0cd6c495b282167acf761729078d975fc341b22650b07b9">>,
+    [?assertEqual({error, [{data, #{}}]}, parse_json(<<"{}">>))
+    ,?assertEqual({ok, #{ <<"names">> => #{ <<"bob">> => BobKey } 
+                        , <<"relays">> => #{}
+                        }}
+                 ,parse_json(<<"{ \"names\": { \"bob\": \"",BobKey/bitstring,"\"} }">>)
+                 )
+    ].
+
+%%--------------------------------------------------------------------
+%% @doc internal.
+%%
+%% Check if the json object returned by the server has a mandatory
+%% attribute called `names'.
+%%
+%% @todo check the content of the names, it must be a string
+%%       containing an hexadecimal encoded public key.
+%%
+%% @end
+%%--------------------------------------------------------------------
+check_json_names(#{ <<"names">> := Names } = Map, Buffer) 
+  when is_map(Names) ->
+    check_json_relays(Map, Buffer#{ <<"names">> => Names });
+check_json_names(Map, _Buffer) ->
+    {error, [{data, Map}]}.
+
+%%--------------------------------------------------------------------
+%% @doc internal.
+%%
+%% Check if the optional attribute `relays' is present in the data
+%% returned by the remote server.
+%%
+%% @todo check the content of each relays, it must be an array
+%%       containing valid nostr URL.
+%% @end
+%%--------------------------------------------------------------------
+check_json_relays(#{ <<"relays">> := Relays }, Buffer)
+  when is_map(Relays) ->
+    Return = Buffer#{ <<"relays">> => Relays },
+    {ok, Return};
+check_json_relays(_, Buffer) ->
+    Return = Buffer#{ <<"relays">> => #{} },
+    {ok, Return}.
+
